3f6a85deea20f3602f79ef53a09e6028225ea337
Valerio <versys.reggentia@gmail.com>
2026-07-12 19:29:58 -0300

    Alteração final da branch

--- tests/test_remotive.py
+++ tests/test_remotive.py
@@ -1,7 +1,24 @@
 from api.remotive import buscar_vagas
 
 
-def test_buscar_vagas_retorna_lista():
+def test_buscar_vagas_retorna_lista(mocker):
+    resposta_falsa = mocker.Mock()
+    resposta_falsa.json.return_value = {
+        "jobs": [
+            {
+                "title": "Data Analyst",
+                "company_name": "Empresa Fictícia",
+            }
+        ]
+    }
+
+    mocker.patch(
+        "api.remotive.requests.get",
+        return_value=resposta_falsa,
+    )
+
     vagas = buscar_vagas("data")
 
     assert isinstance(vagas, list)
+    assert len(vagas) == 1
+    assert vagas[0]["title"] == "Data Analyst"
